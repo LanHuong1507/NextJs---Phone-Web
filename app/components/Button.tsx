8e14bdf5b508b5e2a40962691d86492099d5e938
LanHuong1507 <taejoon1507@gmail.com>
2024-09-27 13:03:24 +0700

    create add to cart

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -22,6 +22,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
+      onClick={onclick}
       disabled={disabled}
       className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 transaition w-full border-slate-700 flex items-center justify-center gap-2 ${
         outline ? "bg-white" : "bg-slate-700"
